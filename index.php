39f219ff54b63f2a1c5d0846429b9801bc0bb201
Ivan <127977316+ivan-developer-01@users.noreply.github.com>
2025-11-12 17:07:44 +0300

    some changes

--- index.php
+++ index.php
@@ -44,6 +44,10 @@ $example_persons_array = [
         'fullname' => 'Шварцнегер Арнольд Густавович',
         'job' => 'babysitter',
     ],
+    [
+        'fullname' => "Иван Колосов Иванович",
+        'job' => "boss"
+    ],
 ];
 
 // $example_persons_array = [
@@ -201,3 +205,56 @@ function getPerfectPartner($surname, $name, $patronymic, $persons) {
 	♡ Идеально на $perfectPercent ♡
 	PERSON;
 }
+
+
+
+
+
+
+
+
+
+// testing functions with example_persons_array
+getPerfectPartner('ИвАнОв', 'ИвАн', 'ИвАнОвИч', $example_persons_array);
+
+foreach ($example_persons_array as $person) {
+	$parts = getPartsFromFullname($person["fullname"]);
+	echo "ФИО: " . $person["fullname"] . "\n";
+	// print_r($parts);
+
+	$fullname = getFullnameFromParts($parts["surname"], $parts["name"], $parts["patronymic"]);
+	print_r($fullname);
+
+	// print_r(getPartsFromFullname(getFullnameFromParts($parts["surname"], $parts["name"], $parts["patronymic"])));
+
+	// Short name function test
+	echo "\n";
+	echo getShortName($fullname);
+
+	// Gender function test
+	echo "\n";
+	$gender_output = "Не удалось определить";
+	$gender_probability = getGenderFromName($fullname);
+	if ($gender_probability === 1) {
+		$gender_output = "Мужчина";
+	}
+	if ($gender_probability === -1) {
+		$gender_output = "Женщина";
+	}
+	echo $gender_output;
+	echo "\n\n";
+	print_r(getPerfectPartner($parts["surname"], $parts["name"], $parts["patronymic"], $example_persons_array));
+
+	echo <<<SPACER
+
+
+	
+	---------------------------------------------------------------
+
+
+
+	SPACER;
+}
+
+
+print_r(getGenderFromNameDescription($example_persons_array));
